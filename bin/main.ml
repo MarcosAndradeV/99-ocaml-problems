99b6129d57cdef19e75ee430b948c729daf76cee
MarcosAndradeV <mastermarcos1212@hotmail.com>
2024-02-22 14:33:54 -0300

    problem 01

--- bin/main.ml
+++ bin/main.ml
@@ -1,9 +1,10 @@
+(* PROBLEM 01 *)
 let rec last (l: 'a list): 'a option =
     match l with
     | [] -> None
     | [x] -> Some x
     | _ :: rest -> last rest
-
+;;
 
 let () =
     print_endline "Hello, World!"
